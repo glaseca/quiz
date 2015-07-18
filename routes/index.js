cf4ea98d9e0c98c587a9b9c7c19840b5e8610dea
glaseca <guillelaseca@gmail.com>
2015-07-19 00:15:38 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,9 @@ router.get('/author', function(req, res) {
   res.render('author');
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
