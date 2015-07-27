b806008a41dc7a3e979c5c216fd2d7dfbafe461d
glaseca <guillelaseca@gmail.com>
2015-07-27 21:08:58 +0200

    Validación en la creación de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -31,7 +31,7 @@ exports.index = function(req, res) {
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  res.render('quizes/show', { quiz: req.quiz});
+  res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -40,7 +40,7 @@ exports.answer = function(req, res) {
   if (req.query.respuesta === req.quiz.respuesta) {
     resultado = 'Correcto';
   }
-  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 // GET /quizes/new
@@ -48,18 +48,24 @@ exports.new = function(req, res){
 	var quiz = models.Quiz.build( // crea objeto quiz
 		{pregunta: "Pregunta", respuesta: "Respuesta"}
 	);
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function(req, res){
 	var quiz = models.Quiz.build(req.body.quiz);
 	
-	// guarda en DB los campos pregunta y respuesta de quiz
-	quiz.save({filds: ["pregunta", "respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	})	// Redirección HTTP (URL relativo) lista de preguntas
-};
-
-
-
+	quiz.validate().then(
+		function(err){
+			if(err){
+				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+			}else{
+				quiz	//save: guarda en DB los campos pregunta y respuesta de quiz
+					.save({filds: ["pregunta", "respuesta"]}).then(
+						function(){
+							res.redirect('/quizes');	// Redirección HTTP (URL relativo) lista de preguntas
+						})
+			}
+		}
+	);
+}
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: [] });
 });
 router.get('/author', function(req, res) {
-  res.render('author');
+  res.render('author', { errors: [] });
 });
 
 // Autoload de comandos con :quizId
