d5dbc4c0328d13037b5371e84ec85b229deb6318
glaseca <guillelaseca@gmail.com>
2015-07-28 20:48:49 +0200

    Edición de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -51,6 +51,32 @@ exports.new = function(req, res){
 	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
+// GET /quizes/:id/edit
+exports.edit = function(req, res){
+	var quiz = req.quiz; // autoload de instancia de quiz
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	
+	req.quiz.validate().then(
+		function(err){
+			if(err){
+				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+			}else{
+				req.quiz	//save: guarda en DB los campos pregunta y respuesta de quiz
+					.save({fields: ["pregunta", "respuesta"]}).then(
+						function(){
+							res.redirect('/quizes');	// Redirección HTTP (URL relativo) lista de preguntas
+						})
+			}
+		}
+	);
+};
+
 // POST /quizes/create
 exports.create = function(req, res){
 	var quiz = models.Quiz.build(req.body.quiz);
@@ -61,11 +87,11 @@ exports.create = function(req, res){
 				res.render('quizes/new', {quiz: quiz, errors: err.errors});
 			}else{
 				quiz	//save: guarda en DB los campos pregunta y respuesta de quiz
-					.save({filds: ["pregunta", "respuesta"]}).then(
+					.save({fields: ["pregunta", "respuesta"]}).then(
 						function(){
 							res.redirect('/quizes');	// Redirección HTTP (URL relativo) lista de preguntas
 						})
 			}
 		}
 	);
-}
+};
