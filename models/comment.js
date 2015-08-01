01dbdbb4937e675863f4611f7a64c65a41f9a49b
glaseca <guillelaseca@gmail.com>
2015-08-01 19:58:37 +0200

    Creación de comentarios

---
+++ models/comment.js
@@ -0,0 +1,12 @@
+// Definicion del modelo de Comment con validacion
+
+module.exports = function(sequelize, DataTypes){
+	return sequelize.define(
+		'Comment',
+		{ texto: {
+			type: DataTypes.STRING,
+			validate: {notEmpty: {msg: "-> Falta comentario"}}
+			}
+		}
+	);
+}
--- models/models.js
+++ models/models.js
@@ -24,7 +24,15 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 var quiz_path = path.join(__dirname, 'quiz');
 var Quiz = sequelize.import(quiz_path);
 
+// Importar la definicion de la tabla Comment
+var comment_path = path.join(__dirname, 'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
+exports.Comment = Comment;
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
