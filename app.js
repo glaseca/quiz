50cc3c9d8f1e09f741d034a01b8af999cdfc2a9f
glaseca <guillelaseca@gmail.com>
2015-08-01 20:34:10 +0200

    Autenticación y sesión de usuario

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
 var methodOverride = require('method-override');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 
@@ -20,10 +21,27 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieParser('Quiz 2015'));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(partials());
+app.use(session({
+    secret: 'Quiz 2015',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Helpers dinamicos:
+app.use(function(req, res, next){
+	// guardar path en session.redir para despues de login
+	if(!req.path.match(/\/login|\/logout/)){
+		req.session.redir = req.path;
+	}
+	
+	// hacer visible req.session en las vistas
+	res.locals.session = req.session;
+	next();
+});
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -11,6 +12,11 @@ router.get('/', function(req, res) {
 router.get('/author', function(req, res) {
   res.render('author', { errors: [] });
 });
+
+// Definicion de rutas de sesion
+router.get('/login', sessionController.new);	// formulario login
+router.post('/login', sessionController.create);	// crear sesion
+router.get('/logout', sessionController.destroy);	// destruir sesion
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
