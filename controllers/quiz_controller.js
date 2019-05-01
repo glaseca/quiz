aecdde3cea019ffa835000a2b94ff4b09d24090c
Guillermo Laseca <guillelaseca@gmail.com>
2019-05-02 00:47:31 +0200

    Change sequelize method

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,7 +2,7 @@ var models = require('../models/models.js');
 
 // Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
-	models.Quiz.find({
+	models.Quiz.findAll({
 		where: {id: Number(quizId)},
 		include: [{ model: models.Comment}]
 	}).then(function(quiz){
